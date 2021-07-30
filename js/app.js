40e9198a9416b3598376e0876886a5dfe0d1379a
esra.alshafi@gmail.com <esra.alshafi@gmail.com>
2021-07-30 21:21:08 +0300

    init repositry

---
+++ js/app.js
@@ -0,0 +1,36 @@
+
+function getAllTodos(){
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(json =>{
+        console.log(json)
+        renderAllTodos(json)
+    
+    } )
+        
+        
+        
+       
+
+}
+
+
+async function renderAllTodos(data) {
+    
+    let html = '';
+    data.forEach(todo => {
+        let htmlSegment = `<tr class="user">
+                            
+                            <td>${todo.userId} </td>
+                            <td class="email">${todo.title}</td>
+                        </tr>`;
+
+        html += htmlSegment;
+    });
+
+    let container = document.querySelector('.container');
+    container.innerHTML = html;
+}
+
+
+getAllTodos()
